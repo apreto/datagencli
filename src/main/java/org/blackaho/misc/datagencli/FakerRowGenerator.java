349bcbbe93a1ce5918e71ef3eb49050504e1babd
Aristides Preto <aristides.preto@hitachivantara.com>
2019-10-15 00:56:17 +0100

    [TD] Refactor FakerRowGenerator

--- src/main/java/org/blackaho/misc/datagencli/FakerRowGenerator.java
+++ src/main/java/org/blackaho/misc/datagencli/FakerRowGenerator.java
@@ -35,7 +35,7 @@ public class FakerRowGenerator implements RowGenerator {
   protected List<String> header;
   protected String headerLine;
   protected String separator;
-  protected List<FieldCacheEntry> fieldCache;
+  protected List<FieldEntry> fieldCache;
 
 
   public FakerRowGenerator() {
@@ -87,8 +87,6 @@ public class FakerRowGenerator implements RowGenerator {
     }
   }
 
-
-
   @Override
   public RowGenerator setHeaderLine(String headerLine) {
     this.headerLine = headerLine;
@@ -100,7 +98,6 @@ public class FakerRowGenerator implements RowGenerator {
     this.header = header;
     return this;
   }
-
 
   @Override
   public RowGenerator setFields(List<String> fields) {
@@ -114,7 +111,6 @@ public class FakerRowGenerator implements RowGenerator {
     return this;
   }
 
-
   @Override
   public String generateHeaderLine() {
     if (this.headerLine!=null) {
@@ -127,25 +123,25 @@ public class FakerRowGenerator implements RowGenerator {
   @Override
   public List generateRow(Long lineNumber) {
     initFieldCache();
-    return fieldCache.stream().map( fieldCacheEntry -> this.getFieldValue(fieldCacheEntry, lineNumber) )
+    return fieldCache.stream().map( fieldEntry -> fieldEntry.getValue(lineNumber) )
         .collect(Collectors.toList());
   }
 
   @Override
   public String generateRowLine(Long lineNumber) {
     initFieldCache();
-    return fieldCache.stream().map( fieldCacheEntry -> this.getFieldValue(fieldCacheEntry, lineNumber).toString() )
+    return fieldCache.stream().map( fieldEntry -> fieldEntry.getValue(lineNumber).toString() )
         .collect(Collectors.joining(this.separator));
   }
 
   protected void initFieldCache() {
     if (fieldCache == null) {
-      fieldCache = fields.stream().map(this::createFieldCacheEntry).collect(Collectors.toList());
+      fieldCache = fields.stream().map( f -> FieldEntry.createFieldEntry(f, this.faker) ).collect(Collectors.toList());
     }
   }
 
 
-  static class FieldCacheEntry {
+  static class FieldEntry {
     String field;
     Object generatorObject;
     Method generatorMethod;
@@ -153,89 +149,93 @@ public class FakerRowGenerator implements RowGenerator {
     enum EntryType { ROWNUMBER, SEQUENCE, FAKER_METHOD}
     EntryType entryType;
 
-    public FieldCacheEntry(String field, EntryType entryType, Object object, Method method, Object[] args) {
+    protected FieldEntry(String field, EntryType entryType, Object object, Method method, Object[] args) {
       this.field = field;
       this.entryType = entryType;
       this.generatorObject = object;
       this.generatorMethod = method;
       this.generatorArguments = args;
     }
-  }
 
-
-  /**
-   * Creates an FieldCacheEntry based on string with field definition. This FieldCacheEntry can be passed to
-   * getFieldValue() to generate a value for the field.
-   * Parses the field definition string (including any arguments) passed as parameter, maps them to
-   * corresponding Faker methods and return a FieldCacheEntry with that..
-   * E.g., randomLong(min:max) is parsed and mapped as faker.number.numberBetween(min,max);
-   *       name.fullName as faker.name().fullName()
-   *
-   * @param field - field definition string
-   * @return a FieldCacheEntry object that can be used with getFieldValue()
-   */
-  protected FieldCacheEntry createFieldCacheEntry(String field) {
-    try {
-      // handle custom fields
-      if (RANDOMSTRING_REGEXP.matcher(field).find()) {
-        Matcher m = RANDOMSTRING_REGEXP.matcher(field); m.find();
-        // simulate faker.bothify(m.group(1))
-        return new FieldCacheEntry(field, FieldCacheEntry.EntryType.FAKER_METHOD, faker,
-            faker.getClass().getDeclaredMethod("bothify", String.class),
-            new Object[] {m.group(1)} );
-      } else if (RANDOMLONGFUNC_REGEXP.matcher(field).find()) {
-        Matcher m = RANDOMLONGFUNC_REGEXP.matcher(field); m.find();
-        // simulate faker.number().numberBetween(Long.parseLong(m.group(1)),Long.parseLong(m.group(2)))
-        return new FieldCacheEntry(field, FieldCacheEntry.EntryType.FAKER_METHOD, faker.number(),
-            faker.number().getClass().getDeclaredMethod("numberBetween", long.class, long.class),
-            new Object[] { Long.parseLong(m.group(1)), Long.parseLong(m.group(2)) } );
-      } else if (RANDOMDOUBLEFUNC_REGEXP.matcher(field).find()) {
-        Matcher m = RANDOMDOUBLEFUNC_REGEXP.matcher(field); m.find();
-        // simulate faker.number().randomDouble(Integer.parseInt(m.group(1)), Long.parseLong(m.group(2)),Long.parseLong(m.group(3)))
-        return new FieldCacheEntry(field, FieldCacheEntry.EntryType.FAKER_METHOD, faker.number(),
-            faker.number().getClass().getDeclaredMethod("randomDouble", int.class, long.class, long.class),
-            new Object[] { Integer.parseInt(m.group(1)), Long.parseLong(m.group(2)),Long.parseLong(m.group(3)) } );
-      } else if (SEQUENCEFUNC_REGEXP.matcher(field).find()) {
-        Matcher m = SEQUENCEFUNC_REGEXP.matcher(field); m.find();
-        return new FieldCacheEntry(field, FieldCacheEntry.EntryType.SEQUENCE,null, null,
-            new Object[] { Long.parseLong(m.group(1)), Long.parseLong(m.group(2)) } );
-      } else if (field.equals("rowNumber")) {
-        return new FieldCacheEntry(field, FieldCacheEntry.EntryType.ROWNUMBER,null, null, null);
-      } else {
-        // handle other fields in field1.name1 format with reflection
-        String[] methodCalls = field.split("\\.");
-        Object objToCall = faker;
-        for (int i = 0; i < methodCalls.length-1; i++) {
-          objToCall = objToCall.getClass().getDeclaredMethod(methodCalls[i]).invoke(objToCall);
+    /**
+     * Creates an FieldEntry based on string with field definition. This FieldEntry can be passed to
+     * getValue() to generate a value for the field.
+     * Parses the field definition string (including any arguments) passed as parameter, maps them to
+     * corresponding Faker methods and return a FieldEntry with that..
+     * E.g., randomLong(min:max) is parsed and mapped as faker.number.numberBetween(min,max);
+     *       name.fullName as faker.name().fullName()
+     *
+     * @param field - field definition string
+     * @return a FieldEntry object that can be used with getValue()
+     */
+    public static FieldEntry createFieldEntry(String field, Faker faker) {
+      try {
+        // handle custom fields
+        if (RANDOMSTRING_REGEXP.matcher(field).find()) {
+          Matcher m = RANDOMSTRING_REGEXP.matcher(field); m.find();
+          // simulate faker.bothify(m.group(1))
+          return new FieldEntry(field, FieldEntry.EntryType.FAKER_METHOD, faker,
+              faker.getClass().getDeclaredMethod("bothify", String.class),
+              new Object[] {m.group(1)} );
+        } else if (RANDOMLONGFUNC_REGEXP.matcher(field).find()) {
+          Matcher m = RANDOMLONGFUNC_REGEXP.matcher(field); m.find();
+          // simulate faker.number().numberBetween(Long.parseLong(m.group(1)),Long.parseLong(m.group(2)))
+          return new FieldEntry(field, FieldEntry.EntryType.FAKER_METHOD, faker.number(),
+              faker.number().getClass().getDeclaredMethod("numberBetween", long.class, long.class),
+              new Object[] { Long.parseLong(m.group(1)), Long.parseLong(m.group(2)) } );
+        } else if (RANDOMDOUBLEFUNC_REGEXP.matcher(field).find()) {
+          Matcher m = RANDOMDOUBLEFUNC_REGEXP.matcher(field); m.find();
+          // simulate faker.number().randomDouble(Integer.parseInt(m.group(1)), Long.parseLong(m.group(2)),Long.parseLong(m.group(3)))
+          return new FieldEntry(field, FieldEntry.EntryType.FAKER_METHOD, faker.number(),
+              faker.number().getClass().getDeclaredMethod("randomDouble", int.class, long.class, long.class),
+              new Object[] { Integer.parseInt(m.group(1)), Long.parseLong(m.group(2)),Long.parseLong(m.group(3)) } );
+        } else if (SEQUENCEFUNC_REGEXP.matcher(field).find()) {
+          Matcher m = SEQUENCEFUNC_REGEXP.matcher(field); m.find();
+          return new FieldEntry(field, FieldEntry.EntryType.SEQUENCE,null, null,
+              new Object[] { Long.parseLong(m.group(1)), Long.parseLong(m.group(2)) } );
+        } else if (field.equals("rowNumber")) {
+          return new FieldEntry(field, FieldEntry.EntryType.ROWNUMBER,null, null, null);
+        } else {
+          // handle other fields in field1.name1 format with reflection
+          String[] methodCalls = field.split("\\.");
+          Object objToCall = faker;
+          for (int i = 0; i < methodCalls.length-1; i++) {
+            objToCall = objToCall.getClass().getDeclaredMethod(methodCalls[i]).invoke(objToCall);
+          }
+          Method methodToCall = objToCall.getClass().getDeclaredMethod(methodCalls[methodCalls.length-1]);
+          return new FieldEntry(field, FieldEntry.EntryType.FAKER_METHOD, objToCall, methodToCall, new Object[] {} );
         }
-        Method methodToCall = objToCall.getClass().getDeclaredMethod(methodCalls[methodCalls.length-1]);
-        return new FieldCacheEntry(field, FieldCacheEntry.EntryType.FAKER_METHOD, objToCall, methodToCall, new Object[] {} );
+      } catch (Exception ex) {
+        return new FieldEntry(field, FieldEntry.EntryType.FAKER_METHOD, null, null, null);
       }
-    } catch (Exception ex) {
-      return new FieldCacheEntry(field, FieldCacheEntry.EntryType.FAKER_METHOD, null, null, null);
     }
+
+    /**
+     * Get a value for this field using reflection on Faker object.
+     * E.g., field "name.firstName" will call faker.name().firstName() and return its value.
+     * @param lineNumber number of the line to generate, importanto for some generator fields.
+     * @return value for corresponding field
+     */
+    protected Object getValue(Long lineNumber) {
+      // handles stuff we can handle directly, w/o faker, line rowNumber and sequence
+      if (this.entryType == FieldEntry.EntryType.ROWNUMBER) {
+        return lineNumber;
+      } else if (this.entryType == FieldEntry.EntryType.SEQUENCE) {
+        return (long) this.generatorArguments[0] + ((lineNumber-1L) * (long) this.generatorArguments[1]);
+      } else {
+        try { // call generator object/method in cache using reflection API
+          return this.generatorMethod.invoke(this.generatorObject, this.generatorArguments);
+        } catch (Exception ex) {
+          return ""; // review - error out instead of return empty string.
+        }
+      }
+    }
+
+
   }
 
-  /**
-   * Get a value for a field using reflection on Faker object, based on method/params defined on fieldCacheEntry.
-   * E.g., field "name.firstName" will call faker.name().firstName() and return its value.
-   * @param fieldCacheEntry object, created with createFieldCacheEntry()
-   * @return value for corresponding field
-   */
-  protected Object getFieldValue(FieldCacheEntry fieldCacheEntry, Long lineNumber) {
-    // handles stuff we can handle directly, w/o faker, line rowNumber and sequence
-    if (fieldCacheEntry.entryType == FieldCacheEntry.EntryType.ROWNUMBER) {
-      return lineNumber;
-    } else if (fieldCacheEntry.entryType == FieldCacheEntry.EntryType.SEQUENCE) {
-      return (long) fieldCacheEntry.generatorArguments[0] + ((lineNumber-1L) * (long) fieldCacheEntry.generatorArguments[1]);
-    } else { // FieldCacheEntry.EntryType.SEQUENCE
-      try { // call generator object/method in cache using reflection API
-        return fieldCacheEntry.generatorMethod.invoke(fieldCacheEntry.generatorObject, fieldCacheEntry.generatorArguments);
-      } catch (Exception ex) {
-        return ""; // review - error out instead of return empty string.
-      }
-    }
-  }
+
+
 
 
 }
